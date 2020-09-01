0029b67f7d61c9ac797edfa717b7fc55ade239b5
dspuertamo <dspuertamo@unal.edu.co>
2020-09-01 17:27:30 -0500

    Añdo ejemplo funcion seno

--- 2020-08-28git/ejemplo2.cpp
+++ 2020-08-28git/ejemplo2.cpp
@@ -1,8 +1,15 @@
+//Primer programa ejemplo c++
+/*
+  dfadf
+  dhds
+  vhfmhcgmhcmc.
+ */
 #include <iostream>
+#include <cmath>
 
 int main(void)
 {
-  std::cout <<"Hello World!\n"
-  std::cout <<" Hola Mundo!\n"; 
+  std::cout <<"Hello World!\n";
+  std::cout <<"Hola Mundo!\n"; 
   return 0;
 }
